5507fe7957ddca6f698488edb010a2408a4f4a50
gitmeoutofhere <aviney702@gmail.com>
2017-03-14 15:45:50 -0700

    Fix javascript for Checkpoint2.8

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,17 @@
-function funcK() {
-    'use strict';
-    var animatePoints = function () {
-            var i,
-                revealPoint,
-                points = document.getElementsByClassName('point');
-            revealPoint = function (index) {
+/* jshint browser: true */
+var animatePoints = function () {
+        var i,
+            revealPoint,
+            points = document.getElementsByClassName('point');
+        revealPoint = function (index) {
                     
-                points[index].style.opacity = 1;
-                points[index].style.transform = "scaleX(1) translateY(0)";
-                points[index].style.msTransform = "scaleX(1) translateY(0)";
-                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-            };
-        
-            for (i = 0; i < points.length; i + 1) {
-                revealPoint(i);
-            }
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
         };
-}
+        
+        for (i = 0; i < points.length; i += 1) {
+            revealPoint(i);
+        }
+    };
