c2addf7425d185ecc14cfb0e12f3454ee96a9075
gitmeoutofhere <aviney702@gmail.com>
2017-03-15 10:19:23 -0700

    Checkpoint 2.9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,30 @@
-/* jshint browser: true */
-var animatePoints = function () {
+/* jshint browser: true, devel: true */
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
+    
         var i,
-            revealPoint,
-            points = document.getElementsByClassName('point');
-        revealPoint = function (index) {
-                    
-            points[index].style.opacity = 1;
-            points[index].style.transform = "scaleX(1) translateY(0)";
-            points[index].style.msTransform = "scaleX(1) translateY(0)";
-            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
+            revealPoint = function (index) {
+                points[index].style.opacity = 1;
+                points[index].style.transform = "scaleX(1) translateY(0)";
+                points[index].style.msTransform = "scaleX(1) translateY(0)";
+                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+            };
         
         for (i = 0; i < points.length; i += 1) {
             revealPoint(i);
         }
     };
+
+window.onload = function () {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function (event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+     });                            
+};
